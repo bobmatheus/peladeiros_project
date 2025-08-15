5de4007ee68ae1487d04e756d9ef5d9135293369
bobmatheus <bob.rtr@hotmail.com>
2025-08-15 06:01:37 -0300

    Define ALLOWED_HOSTS diretamente no settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =========================
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-chave-padrao")
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['peladeiros-br.onrender.com', 'localhost', '127.0.0.1']
 
 # =========================
 # APPS INSTALADOS
